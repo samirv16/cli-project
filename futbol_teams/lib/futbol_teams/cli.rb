7f758388e4fa093dab70cad3fb08e6cce094553e
edras vallecillo <samir.vallecillo@outlook.com>
2019-10-02 19:29:03 +0000

    created an instance method with a case statement that interacts with the user depending on the choice

--- futbol_teams/lib/futbol_teams/cli.rb
+++ futbol_teams/lib/futbol_teams/cli.rb
@@ -1,8 +1,9 @@
 class FutbolTeams::CLI
   
   def call
-    puts "Choose futbol team to see headlines for:"
+    puts "News headlines available for the following 10 futbol teams:"
     list_teams
+    menu
   end
   
   def list_teams
@@ -19,5 +20,33 @@ class FutbolTeams::CLI
       10. Manchester City
       DOC
   end
+  
+  def menu
+    puts "enter number for which team youd like to see news headlines:"
+    
+    input = gets.strip
+    case input 
+      when "1"
+      puts "news headlines for team 1"
+      when "2"
+      puts "news headlines for team 2"
+      when "3"
+      puts "news headlines for team 3"
+      when "4"
+      puts "news headlines for team 4"
+      when "5"
+      puts "news headlines for team 5"
+      when "6"
+      puts "news headlines for team 6"
+      when "7"
+      puts "news headlines for team 7"
+      when "8"
+      puts "news headlines for team 8"
+      when "9"
+      puts "news headlines for team 9"
+      when "10"
+      puts "news headlines for team 10"
+    end
+  end 
   
 end
