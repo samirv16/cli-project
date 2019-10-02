c26fe90beb83e92c60294d8fff54d39154892b93
edras vallecillo <samir.vallecillo@outlook.com>
2019-10-02 19:09:42 +0000

    created a method to list the futbol teams available for news headlines

--- futbol_teams/lib/futbol_teams/cli.rb
+++ futbol_teams/lib/futbol_teams/cli.rb
@@ -2,6 +2,22 @@ class FutbolTeams::CLI
   
   def call
     puts "Choose futbol team to see headlines for:"
+    list_teams
+  end
+  
+  def list_teams
+    puts <<-DOC.gsub /^\s*/, ""
+      1. Real Madrid CF
+      2. FC Barcelona 
+      3. Manchester United
+      4. Chelsea FC
+      5. Bayern Munich
+      6. Arsenal FC 
+      7. PSG
+      8. Juventus
+      9. Liverpool FC
+      10. Manchester City
+      DOC
   end
   
 end
