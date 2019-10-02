0b488663ee6b372cc51d2508700c46b0155687d2
edras vallecillo <samir.vallecillo@outlook.com>
2019-10-02 19:45:14 +0000

    created an instance method to print a goodbye message

--- futbol_teams/lib/futbol_teams/cli.rb
+++ futbol_teams/lib/futbol_teams/cli.rb
@@ -4,6 +4,7 @@ class FutbolTeams::CLI
     puts "News headlines available for the following 10 futbol teams:"
     list_teams
     menu
+    bye
   end
   
   def list_teams
@@ -22,31 +23,37 @@ class FutbolTeams::CLI
   end
   
   def menu
-    puts "enter number for which team youd like to see news headlines:"
-    
-    input = gets.strip
-    case input 
-      when "1"
-      puts "news headlines for team 1"
-      when "2"
-      puts "news headlines for team 2"
-      when "3"
-      puts "news headlines for team 3"
-      when "4"
-      puts "news headlines for team 4"
-      when "5"
-      puts "news headlines for team 5"
-      when "6"
-      puts "news headlines for team 6"
-      when "7"
-      puts "news headlines for team 7"
-      when "8"
-      puts "news headlines for team 8"
-      when "9"
-      puts "news headlines for team 9"
-      when "10"
-      puts "news headlines for team 10"
-    end
+    puts "enter number for which team youd like to see news headlines or type exit to enter:"
+    input = nil
+    while input != "exit"
+      input = gets.strip.downcase
+      case input 
+        when "1"
+        puts "news headlines for team 1"
+        when "2"
+        puts "news headlines for team 2"
+        when "3"
+        puts "news headlines for team 3"
+        when "4"
+        puts "news headlines for team 4"
+        when "5"
+        puts "news headlines for team 5"
+        when "6"
+        puts "news headlines for team 6"
+        when "7"
+        puts "news headlines for team 7"
+        when "8"
+        puts "news headlines for team 8"
+        when "9"
+        puts "news headlines for team 9"
+        when "10"
+        puts "news headlines for team 10"
+      end
+    end 
+  end 
+  
+  def bye
+    puts "Bye Felicia!"
   end 
   
 end
