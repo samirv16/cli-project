6375bc53d7487c51f71fffe9b9509e847e3d5214
edras vallecillo <samir.vallecillo@outlook.com>
2019-10-06 23:07:43 +0000

    clean up

--- futbol_teams/lib/futbol_teams/cli.rb
+++ futbol_teams/lib/futbol_teams/cli.rb
@@ -3,7 +3,7 @@ require 'pry'
 class FutbolTeams::CLI
   
   def call
-    puts "News headlines available for the following 10 futbol teams:"
+    puts "descriptions available for the following top 10 futbol teams of all time:"
     list_teams
     menu
     bye
@@ -20,7 +20,7 @@ class FutbolTeams::CLI
   def menu
     input = nil
     while input != "exit"
-    first_msg = "enter number for which team youd like to see news headlines, type list to see teams again, or type exit:"
+    first_msg = "enter number for which team youd like to see a description for, type 'list' to see teams again, or type exit:"
     puts first_msg
       input = gets.strip.downcase
       
@@ -32,32 +32,7 @@ class FutbolTeams::CLI
       elsif input == "list"
         list_teams
       else
-        puts "That's not an option, please try again:"
-      # case input 
-      #   when "1"
-      #   puts "news headlines for team 1"
-      #   when "2"
-      #   puts "news headlines for team 2"
-      #   when "3"
-      #   puts "news headlines for team 3"
-      #   when "4"
-      #   puts "news headlines for team 4"
-      #   when "5"
-      #   puts "news headlines for team 5"
-      #   when "6"
-      #   puts "news headlines for team 6"
-      #   when "7"
-      #   puts "news headlines for team 7"
-      #   when "8"
-      #   puts "news headlines for team 8"
-      #   when "9"
-      #   puts "news headlines for team 9"
-      #   when "10"
-      #   puts "news headlines for team 10"
-      #   when "list"
-      #     list_teams
-      #   else 
-      #     puts "That's not an option, please try again."
+        puts "That's not an option, please type 'list' to see teamas again, or try again:"
       end
     end 
   end 
--- futbol_teams/lib/futbol_teams/teams.rb
+++ futbol_teams/lib/futbol_teams/teams.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 require 'open-uri'
 
 class FutbolTeams::Teams
-    attr_accessor :name, :description, :scores, :url
+    attr_accessor :name, :url
     
     @doc = Nokogiri::HTML(open("https://howtheyplay.com/team-sports/Top-10-Best-Football-Teams-Of-All-Time"))
   
@@ -29,16 +29,16 @@ class FutbolTeams::Teams
   def self.scrape_tream_desc
     descs = []
     
-    descs << @doc.css("#txtd_22027337 p").text #Spain
-    descs << @doc.css("#txtd_22027334 p").text #Brazil (1970)
-    descs << @doc.css("#txtd_22027332 p").text #Barcelona (2009)
-    descs << @doc.css("#txtd_22027330 p").text #Manchester United (1999)
-    descs << @doc.css("#txtd_22027328 p").text #Real Madrid (1955–1960)
-    descs << @doc.css("#txtd_22027322 p").text #France (1998–2000)
-    descs << @doc.css("#txtd_22027324 p").text #West Germany (1972–1974)
-    descs << @doc.css("#txtd_22027326 p").text #Liverpool (1984)
-    descs << @doc.css("#txtd_22027320 p").text #Milan (1988–1994)
     descs << @doc.css("#txtd_22027317 p").text #Arsenal (2004)
+    descs << @doc.css("#txtd_22027320 p").text #Milan (1988–1994)
+    descs << @doc.css("#txtd_22027326 p").text #Liverpool (1984)
+    descs << @doc.css("#txtd_22027324 p").text #West Germany (1972–1974)
+    descs << @doc.css("#txtd_22027322 p").text #France (1998–2000)
+    descs << @doc.css("#txtd_22027328 p").text #Real Madrid (1955–1960)
+    descs << @doc.css("#txtd_22027330 p").text #Manchester United (1999)
+    descs << @doc.css("#txtd_22027332 p").text #Barcelona (2009)
+    descs << @doc.css("#txtd_22027334 p").text #Brazil (1970)
+    descs << @doc.css("#txtd_22027337 p").text #Spain
     # binding.pry
     
   end
