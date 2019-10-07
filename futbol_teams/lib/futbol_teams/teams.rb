976c1148144e8f4dbfa966cba6a8625e6b026d6c
edras vallecillo <samir.vallecillo@outlook.com>
2019-10-07 17:55:52 +0000

    renamed some variables and checked the program works one last time. Also made sure my program checks all the boxes from the checklist.

--- futbol_teams/lib/futbol_teams/teams.rb
+++ futbol_teams/lib/futbol_teams/teams.rb
@@ -16,13 +16,14 @@ class FutbolTeams::Teams
   def self.scrape_team_names
     
     team_list = @doc.css(".full.module.moduleText .subtitle").text
-    test1 = team_list.split("?").pop
-    test2 = test1.split(/\d\./).drop(1)
-    test3 = test2.shift(9)
-    test4 = test2.pop
-    test5 = test4.split("A")
-    test6 = test5.shift
-    test3 << test6
+    team_list1 = team_list.split("?").pop
+    team_list2 = team_list1.split(/\d\./).drop(1)
+    final_team_list = team_list2.shift(9)
+    team_list4 = team_list2.pop
+    team_list5 = team_list4.split("A")
+    team_list6 = team_list5.shift
+    final_team_list << team_list6
+    final_team_list.reverse
     # binding.pry
   end
      
@@ -30,16 +31,16 @@ class FutbolTeams::Teams
   def self.scrape_tream_desc
     descs = []
     
-    descs << @doc.css("#txtd_22027317 p").text #Arsenal (2004)
-    descs << @doc.css("#txtd_22027320 p").text #Milan (1988–1994)
-    descs << @doc.css("#txtd_22027326 p").text #Liverpool (1984)
-    descs << @doc.css("#txtd_22027324 p").text #West Germany (1972–1974)
-    descs << @doc.css("#txtd_22027322 p").text #France (1998–2000)
-    descs << @doc.css("#txtd_22027328 p").text #Real Madrid (1955–1960)
-    descs << @doc.css("#txtd_22027330 p").text #Manchester United (1999)
-    descs << @doc.css("#txtd_22027332 p").text #Barcelona (2009)
-    descs << @doc.css("#txtd_22027334 p").text #Brazil (1970)
     descs << @doc.css("#txtd_22027337 p").text #Spain
+    descs << @doc.css("#txtd_22027334 p").text #Brazil (1970)
+    descs << @doc.css("#txtd_22027332 p").text #Barcelona (2009)
+    descs << @doc.css("#txtd_22027330 p").text #Manchester United (1999)
+    descs << @doc.css("#txtd_22027328 p").text #Real Madrid (1955–1960)
+    descs << @doc.css("#txtd_22027322 p").text #France (1998–2000)
+    descs << @doc.css("#txtd_22027324 p").text #West Germany (1972–1974)
+    descs << @doc.css("#txtd_22027326 p").text #Liverpool (1984)
+    descs << @doc.css("#txtd_22027320 p").text #Milan (1988–1994)
+    descs << @doc.css("#txtd_22027317 p").text #Arsenal (2004)
     
     
   end
